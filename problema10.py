a374bb65478fd1d72795692d1540799648e1aa2a
ArnoldPardo <arnolmcgiberth0@gmail.com>
2024-07-16 01:34:06 +0000

    subiendo problema10

---
+++ problema10.py
@@ -0,0 +1,10 @@
+# Lista original
+lista = ['Rojo', 'Verde', 'Blanco', 'Negro', 'Rosa', 'Amarillo']
+
+# Eliminar elementos en las posiciones 0, 4 y 5
+del lista[5]  # Eliminar 'Amarillo'
+del lista[4]  # Eliminar 'Rosa'
+del lista[0]  # Eliminar 'Rojo'
+
+# Mostrar la lista resultante
+print("Lista después de eliminar elementos:", lista)
