d9e7747b505783d854ed1e6daa79a3418794fb4a
ArnoldPardo <arnolmcgiberth0@gmail.com>
2024-07-16 01:32:58 +0000

    subiendo problema9

---
+++ problema9.py
@@ -0,0 +1,8 @@
+# Lista original
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+
+# Invertir la lista
+lista_invertida = lista_original[::-1]
+
+# Mostrar la lista invertida
+print("Lista invertida:", lista_invertida)
