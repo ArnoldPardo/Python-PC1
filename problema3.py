a284ba4c5a4c7a48882036e8c61b35f868c4440b
ArnoldPardo <arnolmcgiberth0@gmail.com>
2024-07-16 01:21:25 +0000

    subiendo problema3

---
+++ problema3.py
@@ -0,0 +1,13 @@
+# Definir los pesos de los productos
+peso_payaso = 112  # en gramos
+peso_muneca = 75   # en gramos
+
+# Solicitar el número de payasos y muñecas vendidos
+num_payasos = int(input("Introduce el número de payasos vendidos: "))
+num_munecas = int(input("Introduce el número de muñecas vendidas: "))
+
+# Calcular el peso total
+peso_total = (num_payasos * peso_payaso) + (num_munecas * peso_muneca)
+
+# Mostrar el resultado
+print(f"El peso total del paquete es: {peso_total} gramos")
