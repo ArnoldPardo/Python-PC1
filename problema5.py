04adc15dc024061d5705a579845599cff298b6d6
ArnoldPardo <arnolmcgiberth0@gmail.com>
2024-07-16 01:27:16 +0000

    subiendo problema5

---
+++ problema5.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario el número de shows musicales vistos
+num_shows = int(input("¿Cuántos shows musicales has visto en el último año? "))
+
+# Verificar si ha visto más de 3 shows
+ha_visto_mas_de_tres = num_shows > 3
+
+# Mostrar el resultado
+print(ha_visto_mas_de_tres)
